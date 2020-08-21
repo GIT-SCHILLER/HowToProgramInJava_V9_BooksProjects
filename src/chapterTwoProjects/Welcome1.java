1eab1c590684a31ca7be5f1fbf893c55a0cd3d26
GIT-SCHILLER <gsmsmzadeh@gmail.com>
2020-08-21 20:34:59 +0430

    first push after run.

--- src/chapterTwoProjects/Welcome1.java
+++ src/chapterTwoProjects/Welcome1.java
@@ -1,10 +1,9 @@
 package chapterTwoProjects;
-
+// Fig. 2.1: Welcom1.java
+// Text-printing  program.
 public class Welcome1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		// main method begins execution of Java application.
+		public static void main(String[] args) {
+			System.out.println("Welcome to Java Programming Book!");
+		} // end method main
+	} // end class Welcome
