55e9559bc599fc8af221e7bb85edd6a80cd26b85
GIT-SCHILLER <gsmsmzadeh@gmail.com>
2020-08-22 22:09:48 +0430

    a test commit

--- src/chapterTwoProjects/Welcome1.java
+++ src/chapterTwoProjects/Welcome1.java
@@ -4,6 +4,6 @@ package chapterTwoProjects;
 public class Welcome1 {
 		// main method begins execution of Java application.
 		public static void main(String[] args) {
-			System.out.println("Welcome to Java Programming Book!!");
+			System.out.println("Welcome to Java Programming from Book!!");
 		} // end method main
 	} // end class Welcome
